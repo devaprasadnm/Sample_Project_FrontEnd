7836c233fef3bfe4a30079e59b6d41358e38c3e0
Devaprasad <devaprasadmohan@gmail.com>
2023-02-14 01:33:59 +0530

    Fuction componet implemented

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Hello from './components/Hello';
 
 function App() {
   return (
     <div className="App">
     <h1>Hello World</h1>
+    <Hello/>
         
     </div>
   );
