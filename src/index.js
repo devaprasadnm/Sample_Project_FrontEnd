b4a92b08acbd0d09da0f97d67c5f9caf6857c352
Devaprasad <devaprasadmohan@gmail.com>
2023-02-15 02:11:23 +0530

    react router dom setup

--- src/index.js
+++ src/index.js
@@ -1,13 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import 'bootstrap/dist/css/bootstrap.css';
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import "bootstrap/dist/css/bootstrap.css";
+import { Route, Routes, BrowserRouter as Router } from "react-router-dom";
+import FormComponent from './components/Form';
+
+function Url() {
+  return (
+    <Router>
+          <Routes>
+            <Route path="/" element={<App/>} />
+            <Route path="/login" element={<FormComponent/>} />
+          </Routes>
+    </Router>
+
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Url />
   </React.StrictMode>
 );
 
